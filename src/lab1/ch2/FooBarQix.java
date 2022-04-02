6b548b266477ad66bde3f5bb42cc6869f5c1c0c9
Marian <marianileana95@gmail.com>
2022-04-02 23:28:35 +0300

    FooBarQix Stage2

--- src/lab1/ch2/FooBarQix.java
+++ src/lab1/ch2/FooBarQix.java
@@ -16,7 +16,7 @@ public class FooBarQix {
             l++;
         }
 
-        for (int j = 0; j <= l; j++) {
+        for (int j = l-1; j >= 0; j--) {
             if (digits[j] == 3) s += "Foo";
             else if (digits[j] == 5) s += "Bar";
             else if (digits[j] == 7) s += "Qix";
@@ -27,9 +27,49 @@ public class FooBarQix {
         else return String.valueOf(i);
     }
 
+    public static String compute2(int i){
+        String s = "";
+        boolean ok = true;
+
+        if (i % 3 == 0){
+            ok = false;
+            s += "Foo";
+        }
+        if (i % 5 == 0){
+            ok = false;
+            s += "Bar";
+        }
+        if (i % 7 == 0){
+            ok = false;
+            s += "Qix";
+        }
+
+        int nr, l = 0;
+        int[] digits = new int[100];
+        nr = i;
+        while(nr>0){
+            digits[l] = nr % 10;
+            nr = nr / 10;
+            l++;
+        }
+
+
+        for (int j = l-1; j >= 0; j--) {
+            if (digits[j] == 3) s += "Foo";
+            else if (digits[j] == 5) s += "Bar";
+            else if (digits[j] == 7) s += "Qix";
+            else if (digits[j] == 0) s += "*";
+            else if (ok) s += digits[j];
+        }
+
+        return s;
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(compute(5));
+        System.out.println("compute = " + compute(15));
+
+        System.out.println("compute2 = " + compute2(105));
 
     }
 }
